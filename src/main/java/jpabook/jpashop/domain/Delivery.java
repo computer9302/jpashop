ab3da839375bd71c7cf4887b4cff72d6c63c1f85
computer9302 <computer9302@gmail.com>
2023-01-02 13:22:38 +0900

    스프링 부트, JPA활용 주문 개발(엔티티 클래스, 서비스, 리파지토리)

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,8 +1,12 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter @Setter
 public class Delivery {
     @Id
     @GeneratedValue
@@ -16,6 +20,6 @@ public class Delivery {
     private  Address address;
 
     @Enumerated(EnumType.STRING)
-    private DeleveryStatus status; // READY, COMP
+    private DeliveryStatus status; // READY, COMP
 
 }
